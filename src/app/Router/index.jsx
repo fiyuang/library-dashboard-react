6c5161c310e810540fa743e2d92719d486736d03
fiyuang <fitrianggraini96@gmail.com>
2023-01-30 09:29:21 +0700

    feat: add login supabase auth

--- src/app/Router/index.jsx
+++ src/app/Router/index.jsx
@@ -1,15 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { React, useState, useEffect } from 'react';
 import { Layout } from '../../layout';
 import { Dashboard } from '../../pages/Dashboard';
 import { Login } from '../../pages/Login';
 import { EditBookPage } from '../../pages/EditBookPage';
+
+const DashboardPage = () => {
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // const user = JSON.parse(localStorage.getItem('token'));
+    const value = localStorage.getItem(import.meta.env.VITE_AUTH_KEY_LOCAL_STORAGE);
+
+    if (value) {
+      setIsAuthenticated(true);
+    } else {
+      navigate('/login');
+    }
+  }, []);
+
+  return isAuthenticated ? <Dashboard /> : null;
+};
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<DashboardPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/edit/:bookId" element={<EditBookPage />} />
         </Route>
--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,22 +1,91 @@
-import { Box, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+  Spinner,
+  InputGroup,
+  InputRightElement
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../../utils/supabase';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = () => setShow(!show);
+
+  const handleSubmit = async (e) => {
+    setIsLoading(true);
+
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+      });
+
+      if(data.session) {
+        navigate('/');
+      }
+      setError(JSON.stringify(error.message));
+
+    } catch (error) {
+      setError(error.message);
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Box my={8} textAlign="left">
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" placeholder="Enter your email address" />
-      </FormControl>
-
-      <FormControl mt={4}>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Enter your password" />
-      </FormControl>
-
-      <Button colorScheme="teal" width="full" mt={4}>
-        Sign In
-      </Button>
-    </Box>
+    <>
+      <Box my={8} textAlign="left">
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel>Email address</FormLabel>
+            <Input
+              type="email"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+          <FormControl my={4}>
+            <FormLabel>Password</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={show ? 'text' : 'password'}
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                  {show ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          {error && (
+            <Text color="red" my={3}>
+              {error}
+            </Text>
+          )}
+          <Button type="submit" colorScheme="teal" width="full" mt={4}>
+            {isLoading ? <Spinner mr={2} size="sm" /> : 'Sign In'}
+          </Button>
+        </form>
+      </Box>
+    </>
   );
 };
 
