ba11bd79758b16f550d572dcb554496ddf941432
fiyuang <fitrianggraini96@gmail.com>
2023-02-13 04:37:17 +0700

    feat: init add book data functional

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,15 +2,14 @@ import { Box, Stack } from '@chakra-ui/react';
 import { InputSearch } from '../components/dashboard/InputSearch';
 import { TableDashboard } from '../components/dashboard/TableDashboard';
 import { Navbar } from '../components/common/Navbar';
-import { ButtonAddData } from '../components/dashboard/ButtonAddData';
+import { AddData } from '../components/dashboard/addData/AddData';
 
 const Dashboard = () => {
-
   return (
     <Box h={800}>
       <Navbar />
       <Stack mt={10} spacing={440} direction="row" align="center">
-        <ButtonAddData />
+        <AddData />
         <InputSearch />
       </Stack>
       <TableDashboard />
